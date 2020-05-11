23bd4069628e55a5a66600e32a99519eb3a35d6b
chrisGaye <gayetalib90@gmail.com>
2020-05-11 16:47:23 +0000

    test

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+import 'package:test_app_volkeno/Inscription.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test App from VOLKENO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Inscription()
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),    
+    );
+  }
+}
